11768a5545c1b60b82c91623cf7e427bc49e00be
maratgaliulin <egomarat@gmail.com>
2024-05-30 16:57:11 +1100

    продолжение статистического анализа

--- scripts/basic_stats.py
+++ scripts/basic_stats.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import cudf as pd
+import os
 
 def basic_stats(df:pd.DataFrame, prod_type:str) -> None:
     df = df.drop('product_type', axis=1)
@@ -53,3 +54,61 @@ def basic_stats(df:pd.DataFrame, prod_type:str) -> None:
     for key in results_dict:
         print(key, ':', results_dict[key])
 
+def advertisement_basic_stats(adv_data:pd.DataFrame, campaign_name:str) -> None:
+    
+    # Общая стоимость рекламной кампании в данном разделе
+
+    total_campaign_cost = adv_data['campaign_cost'].sum()
+    
+    # Средняя стоимость кампании на 1 пользователя
+
+    avg_campaign_cost_per_person = round(adv_data.loc[adv_data['campaign_cost'] > 0]['campaign_cost'].sum() / len(adv_data.loc[adv_data['campaign_cost'] > 0]), 3)
+    
+    # Суммы касаний кликов, показов и конверсий
+
+    sum_of_touches = adv_data[[
+        'campaign_type_click', 'campaign_type_show', 'campaign_type_conversion']].sum()
+    
+    # Сумма показов без конверсий
+
+    sum_of_touches_wo_conversions = sum_of_touches['campaign_type_click'] + \
+        sum_of_touches['campaign_type_show']
+        
+    # Процент конверсий от общего числа касаний:
+
+    conversion_touches_percent = round(
+        sum_of_touches['campaign_type_conversion'] / sum_of_touches.sum() * 100, 3)
+    
+    results_dict = {
+        'Сумма показов без конверсий': sum_of_touches_wo_conversions,
+        'Процент конверсий от общего числа касаний': conversion_touches_percent,        
+        f'Общая стоимость рекламной кампании для {campaign_name}': total_campaign_cost,
+        'Средняя стоимость кампании на 1 пользователя': avg_campaign_cost_per_person,
+    }
+    
+    print('--------------------------------------')
+    print(f'Результаты для рекламной кампании {campaign_name}:')
+
+    print('Суммы касаний кликов, показов и конверсий')
+    print('Клики:', sum_of_touches['campaign_type_click'])
+    print('Показы:', sum_of_touches['campaign_type_show'])
+    print('Конверсии:', sum_of_touches['campaign_type_conversion'])
+
+    for key in results_dict:
+        print(key, ':', results_dict[key])
+    
+    print('--------------------------------------')
+    print('\n')
+        
+def parse_directory(directory:str):
+    final_dir = os.fsencode(directory)
+    file_list = []
+    name_list = []
+    
+    for file in os.listdir(final_dir):
+        file_list.append(os.fsdecode(final_dir) + '/' + os.fsdecode(file))
+        name_list.append(os.fsdecode(file)[:-4])
+        
+    for file, name in zip(file_list, name_list):
+        data = pd.read_csv(file)
+        advertisement_basic_stats(data, name)
